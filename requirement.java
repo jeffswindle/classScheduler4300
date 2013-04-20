864f85ae251793c1a7307c3c6f18f582fab3b56b
Jason Beck <beckjfb@uga.edu>
2013-04-20 11:31:41 -0300

    Updated names

--- requirement.java
+++ requirement.java
@@ -2,73 +2,41 @@ package helper;
 
 /**
  * Class that represents a requirement
- * @author Jeffrey Swindle
  */
 public class requirement {
     
     private int reqMapId;
     private String reqCoursePrefix;
     private String reqCourseNumber;
-    
-    /**
-     * Constructor to initialize a new requirement
-     * @param reqID requirement id
-     * @param reqName requirement name
-     * @param reqType requirement type
-     */
-    requirement( int reqID, String reqName, String reqType){
-        this.setReqID(reqID);
-        this.setReqName(reqName);
-        this.setReqType(reqType);
-    }
 
-    /**
-     * Get the requirement ID
-     * @return requirement ID
-     */
-    public int getReqID() {
-        return reqMapId;
-    }
+	public requirement(int reqTypeID, String reqCoursePrefix, String reqCourseNumber) {
+		this.reqMapId = reqTypeID;
+		this.reqCoursePrefix = reqCoursePrefix;
+		this.reqCourseNumber = reqCourseNumber;
+	}
 
-    /**
-     * Set the requirement ID
-     * @param reqID requirement ID
-     */
-    private void setReqID(int reqID) {
-        this.reqMapId = reqID;
-    }
+	public int getReqMapId() {
+		return reqMapId;
+	}
 
-    /**
-     * Set the requirement name
-     * @return requirement name
-     */
-    public String getReqName() {
-        return reqCoursePrefix;
-    }
+	public void setReqMapId(int reqMapId) {
+		this.reqMapId = reqMapId;
+	}
 
-    /**
-     * Set the requirement name
-     * @param reqName requirement name
-     */
-    private void setReqName(String reqName) {
-        this.reqCoursePrefix = reqName;
-    }
+	public String getReqCoursePrefix() {
+		return reqCoursePrefix;
+	}
 
-    /**
-     * Get the requirement type
-     * @return requirement type
-     */
-    public String getReqType() {
-        return reqCourseNumber;
-    }
+	public void setReqCoursePrefix(String reqCoursePrefix) {
+		this.reqCoursePrefix = reqCoursePrefix;
+	}
 
-    /**
-     * Set the requirement type
-     * @param reqType requirement type
-     */
-    private void setReqType(String reqType) {
-        this.reqCourseNumber = reqType;
-    }
-    
-    
+	public String getReqCourseNumber() {
+		return reqCourseNumber;
+	}
+
+	public void setReqCourseNumber(String reqCourseNumber) {
+		this.reqCourseNumber = reqCourseNumber;
+	}
+	
 }
