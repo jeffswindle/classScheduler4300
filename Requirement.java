63e3131e38de3b41c831c9504e5504b649445174
Jason Beck <beckjfb@uga.edu>
2013-04-23 22:37:08 -0300

    Added Comments

--- Requirement.java
+++ Requirement.java
@@ -1,39 +1,46 @@
 package helper;
 
 /**
- * Class that represents a requirement
+ * Class that represents a requirement object
+ * @author Jason Beck
  */
 public class Requirement {
     
-	//Variable Decs
+	//Variable Declarations
     private int reqMapId;
     private String reqCoursePrefix;
     private String reqCourseNumber;
 	private String reqCourseTitle;
-	
+
 	/**
-	*
-	*
-	*/
+	 * Constructor to instantiate a requirement object
+	 * @param reqCoursePrefix course prefix
+	 * @param reqCourseNumber course number
+	 */
 	public Requirement(String reqCoursePrefix, String reqCourseNumber) {
 		this.reqCoursePrefix = reqCoursePrefix;
 		this.reqCourseNumber = reqCourseNumber;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Constructor to instantiate a requirement object
+	 * @param reqTypeID requirement type id
+	 * @param reqCoursePrefix course prefix
+	 * @param reqCourseNumber course number
+	 */
 	public Requirement(int reqTypeID, String reqCoursePrefix, String reqCourseNumber) {
 		this.reqMapId = reqTypeID;
 		this.reqCoursePrefix = reqCoursePrefix;
 		this.reqCourseNumber = reqCourseNumber;
 	}
-	
+
 	/**
-	*
-	*
-	*/
+	 * Constructor to instantiate a requirement object
+	 * @param reqTypeID requirement type id
+	 * @param reqCoursePrefix course prefix
+	 * @param reqCourseNumber course number
+	 * @param reqCourseTitle course title
+	 */
 	public Requirement(int reqTypeID, String reqCoursePrefix, String reqCourseNumber, String reqCourseTitle) {
 		this.reqMapId = reqTypeID;
 		this.reqCoursePrefix = reqCoursePrefix;
@@ -42,67 +49,67 @@ public class Requirement {
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Get the map id
+	 * @return map id
+	 */
 	public int getReqMapId() {
 		return reqMapId;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Set the map id
+	 * @param reqMapId map id
+	 */
 	public void setReqMapId(int reqMapId) {
 		this.reqMapId = reqMapId;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Get the course prefix
+	 * @return course prefix
+	 */
 	public String getReqCoursePrefix() {
 		return reqCoursePrefix;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Set the course prefix
+	 * @param reqCoursePrefix course prefix
+	 */
 	public void setReqCoursePrefix(String reqCoursePrefix) {
 		this.reqCoursePrefix = reqCoursePrefix;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Get the course number
+	 * @return course number
+	 */
 	public String getReqCourseNumber() {
 		return reqCourseNumber;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Set the course number
+	 * @param reqCourseNumber course number
+	 */
 	public void setReqCourseNumber(String reqCourseNumber) {
 		this.reqCourseNumber = reqCourseNumber;
 	}
-	
+
 	/**
-	*
-	*
-	*/
+	 * Get the course title
+	 * @return course title
+	 */
 	public String getReqCourseTitle() {
 		return reqCourseTitle;
 	}
 
 	/**
-	*
-	*
-	*/
+	 * Set the course title
+	 * @param reqCourseTitle course title
+	 */
 	public void setReqCourseTitle(String reqCourseTitle) {
 		this.reqCourseTitle = reqCourseTitle;
 	}
-	
+
 }
