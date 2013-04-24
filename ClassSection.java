236baa0a65de067babba1d1e114dc9bf3315b6b0
Jason Beck <beckjfb@uga.edu>
2013-04-23 22:38:07 -0300

    Added Comments

--- ClassSection.java
+++ ClassSection.java
@@ -2,28 +2,53 @@ package helper;
 
 import java.util.ArrayList;
 
+/**
+ * Class that represents a single section of a course
+ * @author Jason Beck
+ */
 public class ClassSection {
 
+	//Declaration of variables
 	private int callNumber;
 	private ArrayList<ClassMeeting> classMeetingsList;
 	
+	/**
+	 * Constructor to instantiate a ClassMeeting object
+	 * @param callNumber call number of the section
+	 */
 	public ClassSection(int callNumber) {
 		this.callNumber = callNumber;
 		classMeetingsList = new ArrayList<ClassMeeting>();
 	}
 
+	/**
+	 * Get the call number
+	 * @return call number
+	 */
 	public int getCallNumber() {
 		return callNumber;
 	}
 
+	/**
+	 * Set the call number
+	 * @param callNumber call number
+	 */
 	public void setCallNumber(int callNumber) {
 		this.callNumber = callNumber;
 	}
 
+	/**
+	 * Get the ArrayList of meetings that are in this section
+	 * @return ArrayList of ClassMeeting objects
+	 */
 	public ArrayList<ClassMeeting> getClassMeetingsList() {
 		return classMeetingsList;
 	}
 	
+	/**
+	 * Adds a ClassMeeting to the ArrayList of meetings for this section
+	 * @param classMeeting meeting to add to this section
+	 */
 	public void addClassMeetingList(ClassMeeting classMeeting){
 		for(int i=0; i < classMeetingsList.size(); i++){
 			//checks to see if the meeting already exists in the list
